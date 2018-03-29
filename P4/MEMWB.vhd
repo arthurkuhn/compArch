67c50d78f9bf0832574dc78eebdb77d28fdb38cd
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2018-03-29 16:35:17 -0400

    cleanup MEMWB

--- P4/MEMWB.vhd
+++ P4/MEMWB.vhd
@@ -8,109 +8,100 @@ ENTITY MEMWB IS
 			clock			:	IN STD_LOGIC;
 
 			-- Inputs
-			-- For WB
-			inp_RegWrite		:	IN STD_LOGIC;		
-			inp_MemToReg		:	IN STD_LOGIC;
+			RegWriteIn		:	IN STD_LOGIC;		
+			MemToRegIn		:	IN STD_LOGIC;
 
-			-- For ALU
-			inp_zero			: 	IN STD_LOGIC;
-			inp_result			:	IN STD_LOGIC_VECTOR(31 downto 0);
-			inp_HIGH			:	IN STD_LOGIC_VECTOR(31 downto 0);
-			inp_LOW				:	IN STD_LOGIC_VECTOR(31 downto 0);
+			zeroIn			: 	IN STD_LOGIC;
+			resultIn		:	IN STD_LOGIC_VECTOR(31 downto 0);
+			highIn			:	IN STD_LOGIC_VECTOR(31 downto 0);
+			lowIn			:	IN STD_LOGIC_VECTOR(31 downto 0);
 
-			-- For Data Memory
-			inp_wr_done			:	IN STD_LOGIC;
-			inp_rd_ready		:	IN STD_LOGIC;
-			inp_data			:	IN STD_LOGIC_VECTOR(31 downto 0);
+			wrDoneIn		:	IN STD_LOGIC;
+			rdReadyIn		:	IN STD_LOGIC;
+			DataIn			:	IN STD_LOGIC_VECTOR(31 downto 0);
 
-			inp_rd 				:	IN STD_LOGIC_VECTOR(4 downto 0);
+			rdIn 			:	IN STD_LOGIC_VECTOR(4 downto 0);
 
 			--Outputs
-			-- For WB
-			out_RegWrite		:	OUT STD_LOGIC;
-			out_MemToReg		:	OUT STD_LOGIC;
+			RegWriteOut		:	OUT STD_LOGIC;
+			MemToRegOut		:	OUT STD_LOGIC;
 
-			-- For ALU
-			out_zero			: 	OUT STD_LOGIC;
-			out_result			:	OUT STD_LOGIC_VECTOR(31 downto 0);
-			out_HIGH			: 	OUT STD_LOGIC_VECTOR(31 downto 0);
-			out_LOW				:	OUT STD_LOGIC_VECTOR(31 downto 0);
-			
-			--For Data Mem
-			out_wr_done			: 	OUT STD_LOGIC;
-			out_rd_ready		:	OUT STD_LOGIC;
-			out_data			:	OUT STD_LOGIC_VECTOR(31 downto 0);
+			zeroOut			: 	OUT STD_LOGIC;
+			resultOut		:	OUT STD_LOGIC_VECTOR(31 downto 0);
+			highOut			: 	OUT STD_LOGIC_VECTOR(31 downto 0);
+			lowOut			:	OUT STD_LOGIC_VECTOR(31 downto 0);
 
-			out_rd 				: 	OUT STD_LOGIC_VECTOR(4 downto 0);
+			wrDoneOut		: 	OUT STD_LOGIC;
+			rdReadyOut		:	OUT STD_LOGIC;
+			dataOut			:	OUT STD_LOGIC_VECTOR(31 downto 0);
+
+			rdOut 			: 	OUT STD_LOGIC_VECTOR(4 downto 0);
 
 		);
 END MEMWB;
 
-ARCHITECTURE ARCH OF MEMWB IS
+ARCHITECTURE arch OF MEMWB IS
 
--- For WB
-signal tmp_RegWrite		:	STD_LOGIC;
-signal tmp_MemToReg		: 	STD_LOGIC;
+signal RegWriteTmp		:	STD_LOGIC;
+signal MemToRegTmp		: 	STD_LOGIC;
 
---For Data Memory
-signal tmp_wr_done		:	STD_LOGIC;
-signal tmp_rd_ready		:	STD_LOGIC;
-signal tmp_data			: 	STD_LOGIC_VECTOR(31 downto 0);
+signal wrDoneTmp		:	STD_LOGIC;
+signal rdReadyTmp		:	STD_LOGIC;
+signal dataTmp			: 	STD_LOGIC_VECTOR(31 downto 0);
 
---For ALU
-signal tmp_zero			:	STD_LOGIC;
-signal tmp_result		: 	STD_LOGIC_VECTOR(31 downto 0);
-signal tmp_HIGH			:	STD_LOGIC_VECTOR(31 downto 0);
-signal tmp_LOW			:	STD_LOGIC_VECTOR(31 downto 0);
+signal zeroTmp			:	STD_LOGIC;
+signal resultTmp		: 	STD_LOGIC_VECTOR(31 downto 0);
+signal highTmp			:	STD_LOGIC_VECTOR(31 downto 0);
+signal lowTmp			:	STD_LOGIC_VECTOR(31 downto 0);
 
-signal tmp_rd 			:	STD_LOGIC_VECTOR(4 downto 0);
+signal rdTmp 			:	STD_LOGIC_VECTOR(4 downto 0);
 
 BEGIN
-	-- WB
-	tmp_RegWrite <=	inp_RegWrite;
-	tmp_MemToReg <=	inp_MemToReg;
 
-	-- ALU
-	tmp_zero <=	inp_zero;
-	tmp_result <= inp_result;
-	tmp_HIGH <= inp_HIGH;
-	tmp_LOW <= inp_LOW;
+	RegWriteTmp <=	RegWriteIn;
+	MemToRegTmp <=	MemToRegIn;
 
-	-- Data Memory
-	tmp_wr_done <= inp_wr_done;
-	tmp_rd_ready <= inp_rd_ready;
+	zeroTmp <=	zeroIn;
+	resultTmp <= resultIn;
+	highTmp <= highIn;
+	lowTmp <= lowIn;
 
-	tmp_rd <= inp_rd;
+	wrDoneTmp <= wrDoneIn;
+	rdReadyTmp <= rdReadyIn;
+
+	rdTmp <= rdIn;
 
 
-data_buffer: process(inp_data)
+data_buffer: process(dataIn)
+
 begin
-	if(inp_data /= "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") then
-		tmp_data <= inp_data;
+	if(dataIn /= "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU") then
+		dataTmp <= dataIn;
 	end if;
+
 end process;
 
 IDIF: process(clock)
+
 begin 
 	if(clock'event AND clock = '1') then
-		--WB
-		out_RegWrite <= tmp_RegWrite;
-		out_MemToReg <= tmp_MemToReg;
 
-		--ALU
-		out_zero <= tmp_zero;
-		out_result <= tmp_result;
-		out_HIGH <= tmp_HIGH;
-		out_LOW <= tmp_LOW;
+		RegWriteOut <= RegWriteTmp;
+		MemToRegOut <= MemToRegTmp;
 
-		--Data Memory
-		out_wr_done <= tmp_wr_done;
-		out_rd_ready <= tmp_rd_ready;
-		out_data <= tmp_data;
+		zeroOut <= zeroTmp;
+		resultOut <= resultTmp;
+		highOut <= highTmp;
+		lowOut <= lowTmp;
 
-		out_rd <= tmp_rd;
+		wrDoneOut <= wrDoneTmp;
+		rdReadyOut <= rdReadyOut;
+		dataOut <= dataTmp;
+
+		rdOut <= rdTmp;
 
 	end if;
+
 end process;
 
-END ARCH;
+END arch;
