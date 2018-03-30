757c17bb5e6e31e69fde63b703417784c76ae426
Arthur <arthurkuhn@outlook.com>
2018-03-30 16:29:40 -0400

    ALU

---
+++ P4/ALU.vhd
@@ -0,0 +1,92 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+entity ALU is
+	Port (
+		CLK	: in  STD_LOGIC;
+		InA     : in  STD_LOGIC_VECTOR(31 downto 0);
+        InB     : in  STD_LOGIC_VECTOR(31 downto 0);
+        Control : in  STD_LOGIC_VECTOR(3  downto 0);
+        Shamt   : in  STD_LOGIC_VECTOR(4  downto 0);
+        Result  : out STD_LOGIC_VECTOR(31 downto 0);
+        IsZero  : out STD_LOGIC;
+        Hi      : out STD_LOGIC_VECTOR(31 downto 0);
+        Lo      : out STD_LOGIC_VECTOR(31 downto 0);
+	);
+end ALU;
+
+architecture Behavior of ALU is
+
+    signal addResult, subResult, multResult, divResult, divRemainder : integer := 0; -- integer temporary results
+    signal divOut, divRemainderOut, aluOut, sltOut : std_logic_vector(31 downto 0);  -- 32 bit integers
+    signal addSub : integer;
+
+    constant AND_OP : STD_LOGIC_VECTOR (4 downto 0) := "0000";
+    constant OR_OP : STD_LOGIC_VECTOR (4 downto 0) := "0001";
+    constant ADD_OP : STD_LOGIC_VECTOR (4 downto 0) := "0010";
+    constant MULT_OP : STD_LOGIC_VECTOR (4 downto 0) := "0011";
+    constant SUB_OP : STD_LOGIC_VECTOR (4 downto 0) := "0110";
+    constant SET_LT : STD_LOGIC_VECTOR (4 downto 0) := "0111";
+    constant SHIFT_LOGICAL_L : STD_LOGIC_VECTOR (4 downto 0) := "1000";
+    constant SHIFT_LOGICAL_R : STD_LOGIC_VECTOR (4 downto 0) := "1001";
+    constant SHIFT_R : STD_LOGIC_VECTOR (4 downto 0) := "1010";
+    constant XOR_OP : STD_LOGIC_VECTOR (4 downto 0) := "1100";
+    constant NOR_OP : STD_LOGIC_VECTOR (4 downto 0) := "1101";
+
+
+begin
+
+    -- The operations are synthesized first, a multiplexer is then used to select the correct output
+
+    -- INTEGER OPERATIONS --
+    addResult <= to_integer(signed(InA)) + to_integer(signed(InB));
+    subResult <= to_integer(signed(InA)) - to_integer(signed(InB));
+    multiplierResult <= to_integer(signed(InA)) * to_integer(signed(InB));
+    dividerResult	 <= to_integer(signed(InA)) / to_integer(signed(InB)) when to_integer(signed(InB)) /= 0;
+    dividerRemainder <= to_integer(signed(InA)) mod to_integer(signed(InB)) when to_integer(signed(InB)) /= 0;
+
+    -- CONVERSIONS --
+    sltOut <= std_logic_vector(to_signed(subResult, 32)); -- MSB gives the output
+    multOut <= std_logic_vector(to_signed(multiplierResult, 64));
+    divOut <= std_logic_vector(to_signed(dividerResult, 32));
+    divRemainderOut <= std_logic_vector(to_signed(dividerRemainder, 32));
+
+    -- Output Multiplexer
+    with control select
+        aluOut <=
+            InA AND InB                                                             when AND_OP,
+            InA OR  InB 		                                                    when OR_OP,
+            std_logic_vector(to_signed(subResult, 32)) 		                        when SUB_OP,
+            std_logic_vector(to_signed(addResult, 32)) 		                        when ADD_OP,
+            std_logic_vector(unsigned(sltResult) srl 31) 	                        when SET_LT, -- The MSB is 1 if negative of 0 if positve
+            InA NOR InB 								                            when NOR_OP,
+            InA XOR InB 							                                when XOR_OP,
+            std_logic_vector(unsigned(InB) sll to_integer(unsigned(shamt)))	        when SHIFT_LOGICAL_LEFT,
+            std_logic_vector(unsigned(InB) srl to_integer(unsigned(shamt)))	        when SHIFT_LOGICAL_RIGHT,
+            to_stdlogicvector(to_bitvector(InA) sra to_integer(unsigned(shamt)))	when SHIFT,
+            std_logic_vector(to_signed(addResult, 32)) 					            when OTHERS;
+
+    -- Slt Comparaison
+    with subResult select
+    	IsZero <=
+    		'1'                                     when 0,
+    		'0'                                     when OTHERS;
+
+    -- High Output
+    with control select
+    	Hi <=
+    		multResult(63 DOWNTO 32)		        when  "0011",
+    		divRemainder(31 DOWNTO 0)		        when  "0100",
+    		"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"	    when  OTHERS;
+
+    -- Lo Output
+    with control select
+    	Lo <=
+    	    multResult(31 DOWNTO 0) 		        when "0011",
+    		divResult				                when "0100",
+    		"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"	    when OTHERS;
+
+    result <= aluResult;
+
+end Behavioral;
---
+++ P4/ALU_Control.vhd
@@ -0,0 +1,116 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity ALU_control is
+	port (
+		ALUOp			: in std_logic_vector(2 downto 0); -- from main control
+		funct 			: in std_logic_vector(5 downto 0); -- from instruction
+		operation		: out std_logic_vector(3 downto 0); -- output to ALU
+		writeLOHI		: out std_logic;
+		readLOHI		: out std_logic_vector(1 downto 0)
+	);
+end ALU_control;
+
+architecture behaviour of ALU_control is
+	signal op_reg: std_logic_vector(3 downto 0);
+
+    constant AND_OP : STD_LOGIC_VECTOR (4 downto 0) := "0000";
+    constant OR_OP : STD_LOGIC_VECTOR (4 downto 0) := "0001";
+    constant ADD_OP : STD_LOGIC_VECTOR (4 downto 0) := "0010";
+    constant MULT_OP : STD_LOGIC_VECTOR (4 downto 0) := "0011";
+    constant SUB_OP : STD_LOGIC_VECTOR (4 downto 0) := "0110";
+    constant SET_LT : STD_LOGIC_VECTOR (4 downto 0) := "0111";
+    constant SHIFT_LOGICAL_L : STD_LOGIC_VECTOR (4 downto 0) := "1000";
+    constant SHIFT_LOGICAL_R : STD_LOGIC_VECTOR (4 downto 0) := "1001";
+    constant SHIFT_R : STD_LOGIC_VECTOR (4 downto 0) := "1010";
+    constant XOR_OP : STD_LOGIC_VECTOR (4 downto 0) := "1100";
+    constant NOR_OP : STD_LOGIC_VECTOR (4 downto 0) := "1101";
+
+begin
+
+
+	reg_process: process (ALUOp, funct)
+	begin
+	writeLOHI <= '0';
+	readLOHI <= "00";
+	case ALUOp is
+		-- load and store
+		when "000" =>
+			op_reg <= ADD;
+		-- branch equal
+		when "001" =>
+			op_reg <= SUB_OP;
+		-- I-type
+		-- slti
+		when "100" =>
+			op_reg <= SET_LT;
+		-- andi
+		when "101" =>
+			op_reg <= AND_OP;
+		-- ori
+		when "110" =>
+			op_reg <= OR_OP;
+		-- xori
+		when "111" =>
+			op_reg <= XOR_OP;
+		-- R-type
+		when "010" =>
+			case funct is
+				-- AND
+				when "100100"=>
+					op_reg <= AND_OP;
+				-- OR
+				when "100101"=>
+					op_reg <= OR_OP;
+				-- add
+				when "100000"=>
+					op_reg <= ADD_OP;
+				-- multiply
+				when "011000"=>
+					op_reg <= MULT_OP;
+					writeLOHI <= '1';
+				-- divide
+				when "011010"=>
+					op_reg <= DIV_OP;
+					writeLOHI <= '1';
+				-- subtract
+				when "100010"=>
+					op_reg <= SUB_OP;
+				-- set on less than
+				when "101010"=>
+					op_reg <= SET_LT;
+				-- shift left logical
+				when "000000"=>
+					op_reg <= SHIFT_LOGICAL_L
+				-- shift right logical
+				when "000010"=>
+					op_reg <= SHIFT_LOGICAL_R
+				-- shift right arithmetic
+				when "000011"=>
+					op_reg <= SHIFT;
+				-- NOR
+				when "100111"=>
+					op_reg <= NOR_OP;
+				-- XOR
+				when "100110"=>
+					op_reg <= XOR_OP;
+				-- Move from LO
+				when "010010"=>
+					op_reg <= ADD;
+					readLOHI <= "11";
+				-- Move from HI
+				when "010000"=>
+					op_reg <= ADD;
+					readLOHI <= "10";
+				when others =>
+					null;
+			end case;
+		when others =>
+			null;
+	end case;
+	end process;
+
+	operation <= op_reg;
+
+end behaviour;
